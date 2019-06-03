55e70d65f56ee6cdff9922e9fd410183b5b2670d
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-03 11:25:44 -0600

    Added firebase library to wrong place. Yeeesh what a Noob I am. :)

--- GeoChat/App.js
+++ GeoChat/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Adding firebase</Text>
       </View>
     );
   }
