16da9d420d6fd04cd0b9e9f59e83e57887e5a9e6
Drew <drewsup123c@gmail.com>
2019-06-03 15:10:40 -0600

    added simple function to get users coords

--- GeoChat/App.js
+++ GeoChat/App.js
@@ -6,14 +6,37 @@ import * as firebase from 'firebase'
 
 export default class App extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      coords : '',
+    }
+  }
+
   componentDidMount(){
     InitializeFirebase();
+    this.getUsersCoords()
   }
-  
+
+  getUsersCoords = () => {
+    if(navigator.geolocation){
+      // console.log(navigator.geolocation.getCurrentPosition())
+      navigator.geolocation.getCurrentPosition(this.parseCoords)
+    }else{
+      this.setState({coords : "could not find coords"})
+    }
+  }
+
+  parseCoords = position => {
+    this.setState({coords : `${position.coords.latitude} + ${position.coords.longitude}`})
+    console.log(position.coords.latitude, position.coords.longitude)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Adding firebase</Text>
+        <Text>{this.state.coords}</Text>
       </View>
     );
   }
