0166b67296f52f7b90a5b1f769e166a9050340c2
Drew <drewsup123c@gmail.com>
2019-06-03 23:04:43 -0600

    commented some code and got some things ready for authentication

--- GeoChat/components/test.js
+++ GeoChat/components/test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {connect} from 'react-redux';
+// This is just a test component that has redux state mapped to it
+
 
 function Test(props){
     return(
         <View>
+            {/* Just seeing if it can display the text in Redux */}
             <Text>{props.test}</Text>
         </View>
     )
