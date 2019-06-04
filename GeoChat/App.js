ecd87df349f72adc2a9f950a0f3fe6c30b74aede
Drew <drewsup123c@gmail.com>
2019-06-04 11:59:51 -0600

    redux works fully now, react-router works, and sign in anonymously works as well

--- GeoChat/App.js
+++ GeoChat/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-// import { API_KEY } from 'react-native-dotenv'
 import InitializeFirebase from './firebaseConfig';
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 // Redux Imports
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import {reducer} from './Redux/reducers/index';
-import Test from './components/test';
+// React Router and Component Imports
+import {NativeRouter, Route} from 'react-router-native';
+import HomePage from './screens/HomePage';
 
 const store = createStore(
   reducer,
@@ -27,39 +28,16 @@ export default class App extends React.Component {
 
   componentDidMount(){
     InitializeFirebase();
-    this.getUsersCoords()
-  }
-
-  getUsersCoords = () => {
-    if(navigator.geolocation){
-      // console.log(navigator.geolocation.getCurrentPosition())
-      // gets users current coordinates and passes it to parse coords
-      navigator.geolocation.getCurrentPosition(this.parseCoords)
-    }else{
-      this.setState({coords : "Please allow this app to use your location"})
-    }
-  }
-
-  parseCoords = position => {
-    this.setState({coords : `${position.coords.latitude} + ${position.coords.longitude}`})
-    console.log(position.coords.latitude, position.coords.longitude)
   }
 
   render() {
     return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Welcome To GeoChat</Text>
-          <Text>{this.state.coords ? this.state.coords : "Loading Coordinates..."}</Text>
-          <Button 
-            onPress={this.getUsersCoords} 
-            disabled={this.state.coords.length ? false : true} 
-            title="Continue Anonymously" 
-          />
-          <Text>{this.props.test}</Text>
-          <Test />
-        </View>
-      </Provider>
+      <NativeRouter>
+        <Provider store={store}>
+          <Route exact path="/" render={(props) => <HomePage {...props}/>} />
+          <Route path="/chat-list" render={() => <View><Text>Test</Text></View>} />
+        </Provider>
+      </NativeRouter>
     );
   }
 }
