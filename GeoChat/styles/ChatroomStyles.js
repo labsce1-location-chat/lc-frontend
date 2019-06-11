cd11cd7bccd25baad74bffa918461f46b8fb177d
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-11 15:34:28 -0600

    Changed names and added another file

---
+++ GeoChat/styles/ChatroomStyles.js
@@ -0,0 +1,22 @@
+
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+
+    container : {
+        flex:1,
+        marginTop : 30,
+    },
+
+    input: {
+        position: 'relative',
+        fontSize: 15,
+        backgroundColor: 'lightblue',
+        padding: 3,
+        borderRadius: 5,
+    }
+
+
+});
+
+
+export default styles;
---
+++ GeoChat/styles/LoadingScreenStyles.js
@@ -0,0 +1,12 @@
+
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    container : {
+        height:"100%",
+        width : "100%",
+        backgroundColor : "#00B3DA"
+    }
+});
+
+
+export default styles;
