8ebb0292cfdcde568b08206c06f1a2407318268c
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-18 14:45:56 -0600

    Changed lightgray to silver

--- GeoChat/styles/homepageStyles.js
+++ GeoChat/styles/homepageStyles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightgray',
+        backgroundColor: 'silver',
         alignItems: 'center',
         justifyContent: 'center',
 
