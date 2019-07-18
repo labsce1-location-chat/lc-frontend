b66dd002d690299106e9eecd5faa58ba631a5c1f
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-18 09:51:12 -0600

    Added text instead of a label. to the create chatroom title field

--- GeoChat/screens/CreateChatRoom.js
+++ GeoChat/screens/CreateChatRoom.js
@@ -79,7 +79,8 @@ class CreateChatRoom extends React.Component{
         return(
 
             <View style={styles.container}>
-                <TextInput name="roomName" style={styles.textBox} label="Chat Room Name" placeholder="Testing" onChangeText={this.handleChange.bind(this, "roomName")} value={this.state.roomName} maxLength={20} />
+                <Text>Enter Name of Chatroom</Text>
+                <TextInput name="roomName" style={styles.textBox} onChangeText={this.handleChange.bind(this, "roomName")} value={this.state.roomName} maxLength={30} />
                 <Button style={styles.normalButton} title="Choose your chatroom Image" onPress={this.pickImage} />
                 {this.state.roomAvatar ? <Image source={{uri : this.state.roomAvatar}} style={{width:200, height:200}}/> : null}
                 <Button style={styles.normalButton}title="Submit" onPress={this.newRoom} />
