e6353e65e1778e4a3bba3ea4e244943898da96cd
Chad Jemmett <chad.jemmett@gmail.com>
2019-10-10 15:29:30 -0600

    GeoChat part Deux. Trying to see if I can build a new app

---
+++ GeoChat2/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+
+const NavBar = () => {
+  return (
+      <Text>Here's the Navbar</Text>
+      )
+
+}
+
+
+export default NavBar;
