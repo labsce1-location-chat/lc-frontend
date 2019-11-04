5e3b999e3b811990df1f2549d7cc4a06948be7bd
Chad Jemmett <chad.jemmett@gmail.com>
2019-11-03 18:46:54 -0700

    Refactoring the maps for react-native-maps

--- GeoChat/components/ChatList/Map.js
+++ GeoChat/components/ChatList/Map.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {MapView} from 'expo';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
 import {Overlay, Button} from 'react-native-elements';
 import {connect} from 'react-redux';
 import TempLogo from '../../assets/TempLogo.png';
 import {Link} from 'react-router-native';
+
+
 
 class Map extends React.Component{
     constructor(){
@@ -21,59 +24,59 @@ class Map extends React.Component{
         this.setState({chatroom : room})
         this.setState({open : true})
     }
+
     render(){
         return(
-            <View>
-                <MapView
-                    style={{height:400, width:400 }}
-                    initialRegion={{
-                    latitude: this.props.location.lat,
-                    longitude: this.props.location.lon,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                    }}
-                    // showsUserLocation={true}
-                    provider="google"
-                >
-                    <MapView.Marker 
-                        coordinate={{latitude : this.props.location.lat, longitude: this.props.location.lon}}
-                        title="Current Location"
-                        description="Your current location"
-                        color="blue"
-                    />
-                    {this.props.chatrooms
-                    ?
-                    this.props.chatrooms.map(room => 
-                        <MapView.Marker
-                            description={room.description} 
-                            title={room.name} 
-                            coordinate={{latitude : room.lat, longitude: room.lon}}
-                            key={room.id}
-                            // removed to use default map pins
-                            // Leaving it in for future image icons
-                            // image={TempLogo}
-                            onPress={() => this.setChatroom(room)}
-                        />
-                    )
-                    :
-                    null
-                    }
-                </MapView>
+            <View style={mapsStyles.container}>
+              <MapView 
+               style={{height: 400, width: 400}} 
+               initialRegion={{
+                 latitude: this.props.location.lat, 
+                 longitude: this.props.location.lon,
+                 latitudeDelta: 0.0822,
+                 longitudeDelta: 0.0321,
+               }}
+               provider={"google"}
+              >
+              <Marker 
+                coordinate={{latitude : this.props.location.lat, longitude: this.props.location.lon}}
+                pinColor={"green"}
+              />
+              {this.props.chatrooms ?
+                this.props.chatrooms.map(room => 
+                  <Marker
+                    coordinate={{latitude: room.lat, longitude: room.lon}}
+                    title={room.lat}
+                    description={room.description}
+                    key={room.id}
+                  />
+                )
+                  : null
 
-                <Overlay onBackdropPress={() => this.setState({ open: false })} isVisible={this.state.open}>
-                    <View>
-                        <Text>{this.state.chatroom.name}</Text>
-                        <Text>{this.state.chatroom.description}</Text>
-                        <Link to={`/chatroom/${this.state.chatroom.id}`}><Text style={{fontSize : 26}}>Join {this.state.chatroom.name}</Text></Link>
-                    </View>
-                </Overlay>
-                {/* <Overlay isVisible={this.state.open}>
-                    <Text></Text>
-                </Overlay> */}
+              
+              }
+
+              </MapView>
             </View>
         );
     }
 }
+
+const mapsStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+
+
+})
 
 const mapStateToProps = state => {
     return {
